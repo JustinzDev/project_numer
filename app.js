3b83fa85afb1fd02fc25233d3a529ed81126093e
JustinzDev <fullyz1532@gmail.com>
2022-05-29 13:51:19 +0700

    testing

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const path = require('path')
 
 const port = process.env.PORT || 5000
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build/index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname, 'build')))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'build/index.html'))
+//     })
+// }
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 
 app.listen(port, (err) => {
     if(err) return console.log(err)
