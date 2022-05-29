032b4eb5732801e04a4b5338bc0b791b4197dbbe
JustinzDev <fullyz1532@gmail.com>
2022-05-29 13:25:03 +0700

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
 import MainPage from './components/mainpage'
 
 test('keywordDocker', () => {
